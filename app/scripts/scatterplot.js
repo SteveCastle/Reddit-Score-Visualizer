9b1f8a109c7ae758ad1145d1d1586430528b0092
Steve Castle <steve@tigerhaunt.com>
2014-07-18 16:26:13 -0700

    Scatter Plot Chart

--- app/scripts/scatterplot.js
+++ app/scripts/scatterplot.js
@@ -5,8 +5,11 @@ if(!d3.chart) {d3.chart = {};}
 
 d3.chart.table = function(){
 	var data;
-	function chart(container){
-        return false;
+	function chart(svg){
+        var g = svg.append('g').
+        attr('transform', 'translate(27,50)');
+
+        var circles = g.selectAll('circle');
 	}
 
 	chart.data = function(d) {data = d; return chart;};
