a2a79fe2e521ed0dfabf6903f7735bcb35f0d0fb
Steve Castle <steve@tigerhaunt.com>
2014-07-18 19:13:47 -0700

    Rebuilt table chart

--- app/scripts/table.js
+++ app/scripts/table.js
@@ -5,7 +5,27 @@ if(!d3.chart) {d3.chart = {};}
 
 d3.chart.table = function(){
 	var data;
-	function chart(svg){
+	function chart(el){
+		var table = el.append('table').classed('table', true);
+        console.log(data);
+		var rows = table.selectAll('tr.rows')
+		.data(data);
+
+		var newRows = rows.enter().append('tr');
+
+		newRows.append('td')
+		.text(function(d){return d.data.score;});
+
+        newRows.append('td')
+		.append('img')
+		.attr({src: function(d){return d.data.thumbnail;}});
+
+        newRows.append('td')
+		.append('a')
+		.attr({href: function(d){return d.data.url;}})
+		.text(function(d){return d.data.title;});
+
+		console.log(el);
 	}
 
 	chart.data = function(d) {data = d; return chart;};
